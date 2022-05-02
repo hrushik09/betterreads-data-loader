f582464efca5a28b170eb70a0309ef402448de04
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2022-05-02 18:36:51 +0530

    first commit

---
+++ src/main/java/io/hrushik09/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package io.hrushik09.betterreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterreadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
+	}
+
+}
