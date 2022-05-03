2fc55f0be429459da35772d476cf63b9abe826c1
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2022-05-03 21:20:02 +0530

    added all authors to the database

--- src/main/java/io/hrushik09/betterreadsdataloader/MainController.java
+++ src/main/java/io/hrushik09/betterreadsdataloader/MainController.java
@@ -2,23 +2,62 @@ package io.hrushik09.betterreadsdataloader;
 
 import io.hrushik09.betterreadsdataloader.author.Author;
 import io.hrushik09.betterreadsdataloader.author.AuthorRepository;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @Controller
 public class MainController {
     @Autowired
     AuthorRepository authorRepository;
 
+    @Value("${datadump.location.author}")
+    private String authorDumpLocation;
+
+    @Value("${datadump.location.works}")
+    private String worksDumpLocation;
+
+    private void initAuthors() {
+        Path path = Paths.get(authorDumpLocation);
+        try (Stream<String> lines = Files.lines(path)) {
+            lines.forEach(line -> {
+                // Read and parse the line
+                String jsonString = line.substring(line.indexOf("{"));
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonString);
+
+                    // Construct the Author object
+                    Author author = new Author();
+                    author.setName(jsonObject.optString("name"));
+                    author.setPersonalName(jsonObject.optString("personal_name"));
+                    author.setId(jsonObject.optString("key").replace("/authors/", ""));
+
+                    // Persist using Repository
+                    authorRepository.save(author);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initWorks() {
+    }
+
     @PostConstruct
     public void start() {
-        Author author = new Author();
-        author.setId("id2");
-        author.setName("name2");
-        author.setPersonalName("personalName2");
-
-        authorRepository.save(author);
+        initAuthors();
+        initWorks();
     }
 }
