8a7841110d4fe7c186a62696e97c1e09b38cffde
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2022-11-13 19:41:49 +0530

    using railway deployment and test data

--- src/main/java/io/hrushik09/betterreadsdataloader/MainController.java
+++ src/main/java/io/hrushik09/betterreadsdataloader/MainController.java
@@ -62,9 +62,9 @@ public class MainController {
                 .withRegion(Regions.US_EAST_1)
                 .build();
 
-//        test(s3client);
+        test(s3client);
 //        uploadAuthors(s3client);
-        uploadWorks(s3client);
+//        uploadWorks(s3client);
     }
 
     private void test(AmazonS3 s3client) {
